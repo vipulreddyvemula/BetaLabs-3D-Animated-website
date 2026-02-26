f1aea553944668de86aa8bd32cdbef4e03c5e017
Aditya A <adityaarun2006@gmail.com>
2026-02-26 11:16:19 +0530

    Added gsap and keyframe animations

--- src/script.js
+++ src/script.js
@@ -24,11 +24,53 @@ document.querySelectorAll('.cta').forEach(btn => {
 });
 
 // Gallery item hover effect
-document.querySelectorAll('.gallery-item').forEach(item => {
-    item.addEventListener('mouseenter', function() {
-        this.style.transform = 'scale(0.98)';
-    });
-    item.addEventListener('mouseleave', function() {
-        this.style.transform = 'scale(1)';
-    });
-});
+// document.querySelectorAll('.gallery-item').forEach(item => {
+//     item.addEventListener('mouseenter', function() {
+//         this.style.transform = 'scale(0.98)';
+//     });
+//     item.addEventListener('mouseleave', function() {
+//         this.style.transform = 'scale(1)';
+//     });
+// });
+
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('.about-head',{
+    scrollTrigger: ".about-head",
+    y:200,
+    opacity:0,
+    duration:2,
+    ease:"power4.out"
+})
+
+gsap.from(".about-text",{
+    scrollTrigger: ".about-text",
+    x:200,
+    opacity:0,
+    duration:2,
+    ease:"power4.out"
+})
+
+const t1=gsap.timeline({
+    scrollTrigger:{
+        trigger:".services-grid",
+        start:"top 80%", //start: "<trigger-position> <viewport-position>"
+        toggleActions:"play reverse play reverse" //toggleActions: "<onEnter> <onLeave> <onEnterBack> <onLeaveBack>"
+    }
+})
+t1.fromTo(".service-card1",
+    {y:50,opacity:0},
+    {y:0,opacity:1, duration:0.25, ease:"power4.out"},
+    "+=0.15"
+).fromTo(".service-card2",
+    {y:50,opacity:0},
+    {y:0,opacity:1, duration:0.25, ease:"power4.out"},
+    "+=0.15"
+).fromTo(".service-card3",
+    {y:50,opacity:0},
+    {y:0,opacity:1, duration:0.25, ease:"power4.out"},
+    "+=0.15"
+);
+
